edaeb0780ec7b870694e6b7f05970f3cbe00a17e
marcin radwan <marcin.radwan08@gmail.com>
2020-10-21 19:40:11 +0200

    Smartwatch's models have been partly implemented.

--- initial/src/main/java/com/smartgun/Application.java
+++ initial/src/main/java/com/smartgun/Application.java
@@ -3,6 +3,9 @@ package com.smartgun;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import java.awt.Point;
+
+import com.smartgun.model.policeman.*;
 
 @EnableScheduling
 @SpringBootApplication
@@ -10,6 +13,22 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+
+		// TESTS
+		// TODO IN FUTURE: MAKE METHOD TO GENERATING NEW PATROLS (maybe with random names, like Mietek)
+		Navigation navigation1 = new Navigation();
+		SmartWatch smartWatch1 = new SmartWatch(
+				new Point(0, 0),
+				navigation1
+		);
+
+		Patrol patrol1 = new Patrol(
+				smartWatch1,
+				navigation1,
+				new Policeman(true),
+				new Policeman(false)
+				// TODO WHEN X WILL BE ADDED: X connector;
+		);
 	}
 
 }
---
+++ initial/src/main/java/com/smartgun/model/policeman/Patrol.java
@@ -0,0 +1,47 @@
+package com.smartgun.model.policeman;
+
+import java.awt.Point;
+
+import com.smartgun.model.policeman.SmartWatch;
+import com.smartgun.model.policeman.Policeman;
+import com.smartgun.model.policeman.Navigation;
+
+public class Patrol {
+    // Jeden smartwatch na patrol
+    private SmartWatch smartWatch;
+    // Jeden samochód z nawigacją na patrol
+    private Navigation navigation;
+    // Dwóch policjantów na patrol, każdy ma jeden pistolet, ale tylko jeden ma pistolet
+    private Policeman headPoliceman;
+    private Policeman youngerPoliceman;
+
+    // TODO: posiadać pistolety
+    public enum State {
+        PATROL,
+        INTERVENTION,
+        BACK_TO_PATROL,
+        BACKUP
+    }
+
+    public Patrol(
+            SmartWatch smartWatch,
+            Navigation navigation,
+            Policeman headPoliceman,
+            Policeman youngerPoliceman
+     //      , X connector
+    ) {
+     //   smartWatch.addConnectior(connector);
+        this.smartWatch = smartWatch;
+        this.navigation = navigation;
+        this.headPoliceman = headPoliceman;
+        this.youngerPoliceman = youngerPoliceman;
+    }
+
+    public void setTarget(Point point, Patrol.State state) {
+        //
+    }
+
+
+    // TODO: CYKLICZNIE POBIERAJ DANE OD SV
+    //public void action
+}
