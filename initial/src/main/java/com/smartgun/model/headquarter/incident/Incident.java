d15f5d80449aa3e3f256191be0122a6baab82fc2
marcin radwan <marcin.radwan08@gmail.com>
2020-10-28 20:21:35 +0100

    Duration time of Incidents has been added.

--- initial/src/main/java/com/smartgun/model/headquarter/incident/Incident.java
+++ initial/src/main/java/com/smartgun/model/headquarter/incident/Incident.java
@@ -6,10 +6,14 @@ import java.util.Random;
 public class Incident {
     private Point incidentPoint;
     private boolean willBeFire;
+    private int startTime;
+    private int endTime;
 
-    public Incident() {
+    public Incident(int startTime, int durationTime) {
         // TODO: random position
         this.incidentPoint = new Point(1, 1);
+        this.startTime = startTime;
+        this.endTime = startTime + endTime;
         Random rand = new Random();
         int n = rand.nextInt(2);
         // p(A) = 0,5; A - probability of fired incident
@@ -23,5 +27,9 @@ public class Incident {
 
     public boolean isFiredIncident() {
         return this.willBeFire;
+    }
+
+    public int getEndTime() {
+        return this.endTime;
     }
 }
