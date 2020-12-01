65ea581fcac05b74be1a9d54fed4e37f29c0a3b7
marcin radwan <marcin.radwan08@gmail.com>
2020-12-01 14:52:43 +0100

    Get params from client

--- initial/src/main/java/com/smartgun/model/simulation/InitialData.java
+++ initial/src/main/java/com/smartgun/model/simulation/InitialData.java
@@ -2,21 +2,77 @@ package com.smartgun.model.simulation;
 
 public class InitialData {
     private boolean isRandomMap;
+    private Integer patrolsCount;
+    private Integer[] patrolsPerDistrict;
+    private Integer ambulancesCount;
+    private Integer patrolRadius;
+    private Integer[] interventionProbablity;
+    private Integer[] nightInterventionProbablity;
+    private Integer[] interventionDuration;
+    private Integer[] shootingProbablity;
+    private Integer[] interventionToShootingProbablity;
+    private Integer[] shootingDuration;
+    private Integer accuratePolicemanShootProbablity;
+    private Integer accuratePolicemanShootProbablityNight;
+    private Integer accurateAggressorShootProbablity;
+    private Integer accurateAggressorShootProbablityNight;
+    private boolean isDayAndNightSystem;
 
-    public InitialData() {
-    }
-
-    public InitialData(boolean isRandomMap) {
-        this.isRandomMap = isRandomMap;
-    }
+    public InitialData() { }
 
     public boolean getIsRandomMap() {
         return this.isRandomMap;
     }
 
+    public Integer getPatrolsCount() { return this.patrolsCount; }
+
+    public Integer[] getPatrolsPerDistrict() { return this.patrolsPerDistrict; }
+
+    public Integer getAmbulancesCount() { return this.ambulancesCount; }
+
+    public Integer getPatrolRadius() { return this.patrolRadius; }
+
+    public Integer[] getInterventionProbablity() { return this.interventionProbablity; }
+
+    public Integer[] getNightInterventionProbablity() { return this.nightInterventionProbablity; }
+
+    public Integer[] getInterventionDuration() { return this.interventionDuration; }
+
+    public Integer[] getShootingProbablity() { return this.shootingProbablity; }
+
+    public Integer[] getInterventionToShootingProbablity() { return this.interventionToShootingProbablity; }
+
+    public Integer[] getShootingDuration() { return this.shootingDuration; }
+
+    public Integer getAccuratePolicemanShootProbablity() { return this.accuratePolicemanShootProbablity; }
+
+    public Integer getAccuratePolicemanShootProbablityNight() { return this.accuratePolicemanShootProbablityNight; }
+
+    public Integer getAccurateAggressorShootProbablity() { return this.accurateAggressorShootProbablity; }
+
+    public Integer getAccurateAggressorShootProbablityNight() { return this.accurateAggressorShootProbablityNight; }
+
+    public boolean getIsDayAndNightSystem() { return this.isDayAndNightSystem; }
+
+
     public String toString() {
         return "{" +
-                     "\'isRandomMap\':" +  this.getIsRandomMap()
+                    "\'patrolsCount\':" +  this.getPatrolsCount() + "," +
+                     "\'isRandomMap\':" +  this.getIsRandomMap() + "," +
+                    "\'patrolsPerDistrict\':" + this.getPatrolsPerDistrict() + "," +
+                    "\'ambulancesCount\':" + this.getAmbulancesCount() + "," +
+                    "\'patrolRadius\':" + this.getPatrolRadius() + "," +
+                    "\'interventionProbablity\':" + this.getInterventionProbablity() + "," +
+                    "\'nightInterventionProbablity\':" + this.getNightInterventionProbablity() + "," +
+                    "\'interventionDuration\':" + this.getInterventionDuration() + "," +
+                    "\'shootingProbablity\':" + this.getShootingProbablity() + "," +
+                    "\'interventionToShootingProbablity\':" + this.getInterventionToShootingProbablity() + "," +
+                    "\'shootingDuration\':" + this.getShootingDuration() + "," +
+                    "\'accuratePolicemanShootProbablity\':" + this.getAccuratePolicemanShootProbablity() + "," +
+                    "\'accuratePolicemanShootProbablityNight\':" + this.getAccuratePolicemanShootProbablityNight() + "," +
+                    "\'accurateAggressorShootProbablity\':" + this.getAccurateAggressorShootProbablity() + "," +
+                    "\'accurateAggressorShootProbablityNight\':" + this.getAccurateAggressorShootProbablityNight() + "," +
+                    "\'isDayAndNightSystem\':" + this.getIsDayAndNightSystem()
                 + "}";
     }
 }
--- initial/src/main/resources/static/js/app.js
+++ initial/src/main/resources/static/js/app.js
@@ -47,12 +47,59 @@ const disconnect = () => {
 };
 
 const login = (data) => {
-    if(!data) {
+    // === COMPLETE DATA FROM FORM ===
+    if (!data) {
         lastData = JSON.stringify({
+            'patrolsCount': $('#countOfPatrols').val() != "" ? parseInt($('#countOfPatrols').val()) : -1,
+            'patrolsPerDistrict': [
+                $('#countOfPatrolsPerSafety').val() != "" ? parseInt($('#countOfPatrolsPerSafety').val()) : -1,
+                $('#countOfPatrolsPerMedium').val() != "" ? parseInt($('#countOfPatrolsPerMedium').val()) : -1,
+                $('#countOfPatrolsPerDangerous').val() != "" ? parseInt($('#countOfPatrolsPerDangerous').val()) : -1
+            ],
+            'patrolRadius': $('#patrolRadius').val() != "" ? parseInt($('#patrolRadius').val()) : -1,
+            'ambulancesCount': $('#countOfAmbulances').val() != "" ? parseInt($('#countOfAmbulances').val()) : -1,
+            'interventionProbablity': [
+                $('#interventionProbablityForSafety').val() != "" ? parseInt($('#interventionProbablityForSafety').val()) : -1,
+                $('#interventionProbablityForMedium').val() != "" ? parseInt($('#interventionProbablityForMedium').val()) : -1,
+                $('#interventionProbablityForDangerous').val() != "" ? parseInt($('#interventionProbablityForDangerous').val()) : -1
+            ],
+            'nightInterventionProbablity': [
+                $('#interventionProbablityForSafetyNight').val() != "" ? parseInt($('#interventionProbablityForSafetyNight').val()) : -1,
+                $('#interventionProbablityForMediumNight').val() != "" ? parseInt($('#interventionProbablityForMediumNight').val()) : -1,
+                $('#interventionProbablityForDangerousNight').val() != "" ? parseInt($('#interventionProbablityForDangerousNight').val()) : -1
+            ],
+            'interventionDuration': [
+                $('#minIntervationDuration').val() != "" ? parseInt($('#minIntervationDuration').val()) : -1,
+                $('#maxIntervationDuration').val() != "" ? parseInt($('#maxIntervationDuration').val()) : -1
+            ],
+            'shootingProbablity': [
+                $('#shootingProbablityForSafety').val() != "" ? parseInt($('#shootingProbablityForSafety').val()) : -1,
+                $('#shootingProbablityForMedium').val() != "" ? parseInt($('#shootingProbablityForMedium').val()) : -1,
+                $('#shootingProbablityForDangerous').val() != "" ? parseInt($('#shootingProbablityForDangerous').val()) : -1
+            ],
+            'interventionToShootingProbablity': [
+                $('#interventionToShootingProbablityForSafety').val() != "" ? parseInt($('#interventionToShootingProbablityForSafety').val()) : -1,
+                $('#interventionToShootingProbablityForMedium').val() != "" ? parseInt($('#interventionToShootingProbablityForMedium').val()): -1,
+                $('#interventionToShootingProbablityForDangerous').val() != "" ? parseInt($('#interventionToShootingProbablityForDangerous').val()) : -1
+            ],
+            'shootingDuration': [
+                $('#minShootingDuration').val() != "" ? parseInt($('#minShootingDuration').val()) : -1,
+                $('#maxShootingDuration').val() != "" ? parseInt($('#maxShootingDuration').val()) : -1
+            ],
+            'accuratePolicemanShootProbablity':
+                $('#accurateShootingProbablityForPoliceman').val() != "" ? parseInt($('#accurateShootingProbablityForPoliceman').val()) : -1,
+            'accuratePolicemanShootProbablityNight':
+                $('#accurateShootingProbablityForPolicemanNight').val() != "" ? parseInt($('#accurateShootingProbablityForPolicemanNight').val()) : -1,
+            'accurateAggressorShootProbablity':
+                $('#accurateShootingProbablityForAggressor').val() != "" ? parseInt($('#accurateShootingProbablityForAggressor').val()) : -1,
+            'accurateAggressorShootProbablityNight':
+                $('#accurateShootingProbablityForAggressorNight').val() != "" ?  parseInt($('#accurateShootingProbablityForAggressorNight').val()) : -1,
+            'isDayAndNightSystem': $('#dayAndNight').is(':checked'),
             'isRandomMap': $('#randMap').is(':checked')
         });
     }
     stompClient.send('/app/login', {}, lastData);
+    console.log("Last data", lastData);
 };
 
 const sendName = () => {
@@ -64,6 +111,10 @@ const showGreeting = (message) => {
 };
 
 $(() => {
+    $("#dayAndNight").change(() => {
+        console.log('Day and Night param');
+        $('.dependsOnNightParam').toggle();
+    });
     $("form").on('submit', (e) => {
         e.preventDefault();
     });
