41cfc5476612080d416eaee76f5d223b7f923c0f
marcin radwan <marcin.radwan08@gmail.com>
2020-12-04 21:01:12 +0100

    Revert "maps divided by sectors"
    This reverts commit 906068edb6eac508ddc32f1a33c988e3ca092acb.

--- initial/src/main/java/com/smartgun/model/map/Maps.java
+++ initial/src/main/java/com/smartgun/model/map/Maps.java
@@ -16,22 +16,12 @@ public class Maps {
             new Map(
                     "samplemap.txt",
                     new ArrayList(Arrays.asList(
-                            new Sector(1, SectorType.RED, new Point(0,0), new Point(10,30)),
-                            new Sector(2, SectorType.YELLOW, new Point(0,31), new Point(10,60)),
-                            new Sector(3, SectorType.YELLOW, new Point(11,0), new Point(20,30)),
-                            new Sector(4, SectorType.GREEN, new Point(11,31), new Point(20,60)),
-                            new Sector(5, SectorType.GREEN, new Point(21,0), new Point(30,30)),
-                            new Sector(6, SectorType.YELLOW, new Point(21,30), new Point(30,60))
-                    ))
-            ),
-            new Map(
-                    "map2.txt",
-                    new ArrayList<>(Arrays.asList(
-                            new Sector(1,SectorType.RED, new Point(0,0), new Point(30,39)),
-                            new Sector(2,SectorType.RED, new Point(0,31), new Point(30,78)),
-                            new Sector(3,SectorType.YELLOW, new Point(31,0), new Point(51,78)),
-                            new Sector(4,SectorType.GREEN, new Point(52,0), new Point(80,39)),
-                            new Sector(5,SectorType.GREEN, new Point(52,40), new Point(80,78))
+                            new Sector(1, SectorType.RED, new Point(0,0), new Point(30,10)),
+                            new Sector(2, SectorType.YELLOW, new Point(31,0), new Point(60,10)),
+                            new Sector(3, SectorType.YELLOW, new Point(0,11), new Point(30,20)),
+                            new Sector(4, SectorType.GREEN, new Point(31,11), new Point(60,20)),
+                            new Sector(5, SectorType.GREEN, new Point(0,21), new Point(30,30)),
+                            new Sector(6, SectorType.YELLOW, new Point(31,21), new Point(60,30))
                     ))
             )
     ));
