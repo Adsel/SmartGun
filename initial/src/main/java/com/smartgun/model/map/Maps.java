50a9bcf8a825f3a073cac30d2857fa38fce98e65
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2020-12-09 15:28:41 +0100

    all 3 maps divided by sectors

--- initial/src/main/java/com/smartgun/model/map/Maps.java
+++ initial/src/main/java/com/smartgun/model/map/Maps.java
@@ -14,22 +14,32 @@ public class Maps {
     public static List<Map> AVAILABLE_MAPS = new ArrayList<>(Arrays.asList(
             new Map(
                     "map1.txt",
-                    new ArrayList(Arrays.asList(
+                    new ArrayList<>(Arrays.asList(
+                            new Sector(1,SectorType.RED, new Point(0,0), new Point(19,39)),
+                            new Sector(2,SectorType.RED, new Point(0,40), new Point(19,78)),
+                            new Sector(3,SectorType.YELLOW, new Point(20,0), new Point(40,39)),
+                            new Sector(4,SectorType.YELLOW, new Point(20,40), new Point(40,78)),
+                            new Sector(5,SectorType.GREEN, new Point(41,0), new Point(60,78))
                     ))
             ),
             new Map(
                     "map2.txt",
                     new ArrayList<>(Arrays.asList(
                             new Sector(1,SectorType.RED, new Point(0,0), new Point(30,39)),
-                            new Sector(2,SectorType.RED, new Point(0,31), new Point(30,78)),
+                            new Sector(2,SectorType.RED, new Point(0,40), new Point(30,78)),
                             new Sector(3,SectorType.YELLOW, new Point(31,0), new Point(51,78)),
-                            new Sector(4,SectorType.GREEN, new Point(52,0), new Point(80,39)),
-                            new Sector(5,SectorType.GREEN, new Point(52,40), new Point(80,78))
+                            new Sector(4,SectorType.YELLOW, new Point(52,0), new Point(79,39)),
+                            new Sector(5,SectorType.GREEN, new Point(52,40), new Point(79,78))
                     ))
             ),
             new Map(
                     "map3.txt",
-                    new ArrayList(Arrays.asList(
+                    new ArrayList<>(Arrays.asList(
+                            new Sector(1,SectorType.RED, new Point(0,0), new Point(29,49)),
+                            new Sector(2,SectorType.RED, new Point(0,50), new Point(29,99)),
+                            new Sector(3,SectorType.YELLOW, new Point(30,0), new Point(58,49)),
+                            new Sector(4,SectorType.YELLOW, new Point(30,50), new Point(58,99)),
+                            new Sector(5,SectorType.GREEN, new Point(59,0), new Point(88,99))
                     ))
             )
     ));
