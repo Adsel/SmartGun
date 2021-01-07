b5327ff52cbb98f955351a7380e735a0863c48cc
Arkadiusz Grzywa <arrrusssdb@gmail.com>
2021-01-07 12:21:14 +0100

    mortality repaired

--- initial/src/main/java/com/smartgun/scheduler/Scheduler.java
+++ initial/src/main/java/com/smartgun/scheduler/Scheduler.java
@@ -85,10 +85,6 @@ public class Scheduler {
         return generator.nextInt(CERTAINTY_PROBABILITY) < percent;
     }
 
-    private boolean isPolicemanKilled() {
-        return generator.nextInt() <= 10;
-    }
-
     private boolean checkIfWillBeShooting(int percent) {
         return generator.nextInt(CERTAINTY_PROBABILITY) < percent;
     }
@@ -242,17 +238,11 @@ public class Scheduler {
             if (type == Event.EventType.AGGRESSOR_HURTED) {
                 description += " aggressor has been hurt";
             } else if (type == Event.EventType.POLICEMAN_HURTED) {
-                description += " policeman has been hurt";
-
-                System.out.println(description);
-                if (isPolicemanKilled()) {
-                    if (type == Event.EventType.POLICEMAN_KILLED) {
+                if (isAccident(Data.serverSimulationData.PROBABILITY_OF_MORTALITY)) {
                         description += " policeman has been killed";
-                        System.out.println(description);
                     } else {
                         description += " policeman has been hurt";
                     }
-                }
             }
 
             Data.serverSimulationData.addEvent(new Event(
